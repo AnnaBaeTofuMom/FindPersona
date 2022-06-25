84fa65c1455de41d07abcf4e7bf1910d42c0c57b
JD-man <ekgkzmtm@gmail.com>
2022-06-26 00:25:47 +0900

    Main 스토리보드 삭제 세팅

---
+++ FindPersona/View/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  FindPersona
+//
+//  Created by JD_MacMini on 2022/06/26.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+}
